29bc6d63cf336be481fc93d590d495589479fdff
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-05-29 13:23:25 +0200

    added id field to SpecialCard and removed magic numbers in test

--- src/test/scala/unibo/pps/wizard/model/TestCard.scala
+++ src/test/scala/unibo/pps/wizard/model/TestCard.scala
@@ -8,14 +8,17 @@ class TestCard extends AnyFunSuite with Matchers:
 
 	test("Color enum contains the four colors"):
 		val colors = Color.values.toList
+		val colors_number = colors.size
 		colors should contain allOf (Color.Blue, Color.Green, Color.Red, Color.Yellow)
-		colors.length shouldBe 4
+		colors.length shouldBe colors_number
 
 	test("Rank enum values range from 1 to 13 and expose correct numeric values"):
 		val rankValues = Rank.values.map(_.value).toList.sorted
-		rankValues shouldBe (1 to 13).toList
+		val min_value = 1
+		val max_value = 13
+		rankValues shouldBe (min_value to max_value).toList
 
-	test("apply(suit, rank) creates a Standard card with correct color and rank"):
+	test("apply(color, rank) creates a Standard card with correct color and rank"):
 		val c = Card(Color.Blue, Rank.Ten)
 		c match
 			case Standard(s, r) =>
@@ -24,8 +27,10 @@ class TestCard extends AnyFunSuite with Matchers:
 			case other => fail(s"Expected Standard card but got: $other")
 
 	test("wizard and jester factory methods return the special cards"):
-		wizard shouldBe Wizard
-		jester shouldBe Jester
+		val wizard_id = 1
+		val jester_id = 2
+		wizard(wizard_id) shouldBe Wizard(wizard_id)
+		jester(jester_id) shouldBe Jester(jester_id)
 
 	test("Standard cards with same color and rank are equal; different ones are not"):
 		val a = Card(Color.Green, Rank.One)
