5b03748919f4fda6d662340ba4f16266583936c0
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-06-04 19:45:09 +0200

    test: refactor TestCard into AnyWordSpec style

--- src/test/scala/it/unibo/pps/wizard/model/TestCard.scala
+++ src/test/scala/it/unibo/pps/wizard/model/TestCard.scala
@@ -1,40 +1,45 @@
 package it.unibo.pps.wizard.model
 
-import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class TestCard extends AnyFunSuite with Matchers:
+class TestCard extends AnyWordSpec with Matchers:
 	import Card.*
 
-	test("Color enum contains the four colors"):
-		val colors = Color.values.toList
-		val colors_number = colors.size
-		colors should contain allOf (Color.Blue, Color.Green, Color.Red, Color.Yellow)
-		colors.length shouldBe colors_number
+	"Color enum" should:
+		"contain the four colors" in:
+			val colors = Color.values.toList
+			val colors_number = colors.size
+			colors should contain allOf (Color.Blue, Color.Green, Color.Red, Color.Yellow)
+			colors.length shouldBe colors_number
 
-	test("Rank enum values range from 1 to 13 and expose correct numeric values"):
-		val rankValues = Rank.values.map(_.value).toList.sorted
-		val min_value = 1
-		val max_value = 13
-		rankValues shouldBe (min_value to max_value).toList
+	"Rank enum values" should:
+		"range from 1 to 13 and expose correct numeric values" in:
+			val rankValues = Rank.values.map(_.value).toList.sorted
+			val min_value = 1
+			val max_value = 13
+			rankValues shouldBe (min_value to max_value).toList
 
-	test("apply(color, rank) creates a Standard card with correct color and rank"):
-		val c = Card(Color.Blue, Rank.Ten)
-		c match
-			case Standard(s, r) =>
-				s shouldBe Color.Blue
-				r shouldBe Rank.Ten
-			case other => fail(s"Expected Standard card but got: $other")
+	"apply(color, rank)" should:
+		"create a Standard card with correct color and rank" in:
+			val c = Card(Color.Blue, Rank.Ten)
+			c match
+				case Standard(s, r) =>
+					s shouldBe Color.Blue
+					r shouldBe Rank.Ten
+				case other => fail(s"Expected Standard card but got: $other")
 
-	test("wizard and jester factory methods return the special cards"):
-		val wizard_id = 1
-		val jester_id = 2
-		wizard(wizard_id) shouldBe Wizard(wizard_id)
-		jester(jester_id) shouldBe Jester(jester_id)
+	"wizard and jester factory methods" should:
+		"return the special cards" in:
+			val wizard_id = 1
+			val jester_id = 2
+			wizard(wizard_id) shouldBe Wizard(wizard_id)
+			jester(jester_id) shouldBe Jester(jester_id)
 
-	test("Standard cards with same color and rank are equal; different ones are not"):
-		val a = Card(Color.Green, Rank.One)
-		val b = Card(Color.Green, Rank.One)
-		val c = Card(Color.Yellow, Rank.One)
-		a shouldBe b
-		a should not be c
+	"Standard cards equality" should:
+		"treat cards with same color and rank as equal and different ones as not equal" in:
+			val a = Card(Color.Green, Rank.One)
+			val b = Card(Color.Green, Rank.One)
+			val c = Card(Color.Yellow, Rank.One)
+			a shouldBe b
+			a should not be c
