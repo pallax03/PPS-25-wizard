a47ee9133c2b8bef195b9b8e9e7b8291cd49ae72
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-06-17 23:48:36 +0200

    feat: added vert.x dependency and implemented VerticleExecutor

---
+++ src/test/scala/it/unibo/pps/wizard/util/vertx/TestVerticleExecutor.scala
@@ -0,0 +1,24 @@
+package it.unibo.pps.wizard.util.vertx
+
+import io.vertx.core.Vertx
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.Await
+import scala.concurrent.duration.{Duration, SECONDS}
+
+class TestVerticleExecutor extends AnyWordSpec with Matchers:
+  private val verticleExecutor: VerticleExecutor = VerticleExecutor(Vertx.vertx())
+  private val numberOfTasks: Long = 1000
+  private val maxDuration: Duration = Duration(5, SECONDS)
+
+  "A verticle executor" should:
+    "execute all pending tasks in the same event-loop thread" in:
+      java.util.stream.Stream
+        .iterate(0, _ + 1)
+        .limit(numberOfTasks)
+        .parallel()
+        .map(_ => verticleExecutor.runLater { Thread.currentThread().threadId })
+        .map(Await.result(_, maxDuration))
+        .distinct()
+        .count() shouldBe 1
