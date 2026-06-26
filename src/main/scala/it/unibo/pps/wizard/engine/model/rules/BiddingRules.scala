756a4f97b95e5875102ea0a9c2cb0b9d5666a25e
Francesco Marcatelli <francesco.marcatelli@studio.unibo.it>
2026-06-26 18:27:30 +0200

    feat: implement pure bidding rules and validation logic and corresponding test

---
+++ src/main/scala/it/unibo/pps/wizard/engine/model/rules/BiddingRules.scala
@@ -0,0 +1,27 @@
+package it.unibo.pps.wizard.engine.model.rules
+
+import it.unibo.pps.wizard.engine.model.basic.*
+
+trait BiddingRules:
+  def processBid(bid: Bid, currentBids: BidsCollection, currentPlayer: PlayerId, round: Round, totalPlayers: Int): Either[GameError, BidsCollection]
+
+object BiddingRules:
+
+  def apply(): BiddingRules = new StandardWizardBiddingRules
+
+  private class StandardWizardBiddingRules extends BiddingRules:
+
+    override def processBid(bid: Bid, currentBids: BidsCollection, currentPlayer: PlayerId, round: Round, totalPlayers: Int): Either[GameError, BidsCollection] =
+      if isBidValid(bid, round, currentBids, totalPlayers)
+      then
+        val updatedBids = currentBids + (currentPlayer -> bid)
+        Right(updatedBids)
+      else Left(GameError.InvalidBid)
+
+    private def isBidValid(bid: Bid, round: Round, currentBids: BidsCollection, totalPlayers: Int): Boolean =
+      val isWithinBounds = bid >= Bid.zero && bid <= round
+      val isLastPlayer = currentBids.size == totalPlayers - 1
+
+      val isHookValid = !isLastPlayer || (currentBids.sum + bid) != round
+
+      isWithinBounds && isHookValid
