23c33f66f560b1c016d3c5d9f49b601e2a53805d
alex mazzoni <alex.mazzoni3@studio.unibo.it>
2026-06-26 16:15:25 +0200

    feat: DSL for Table and TableRules

--- src/main/scala/it/unibo/pps/wizard/engine/model/core/GameError.scala
+++ src/main/scala/it/unibo/pps/wizard/engine/model/core/GameError.scala
@@ -1,7 +1,13 @@
 package it.unibo.pps.wizard.engine.model.core
 
+import it.unibo.pps.wizard.engine.model.basic.Card
+
+enum Reasons:
+  case CardNotInHand
+  case MustFollowLeader(requiredColor: Card.Color)
+
 enum GameError:
   case NotYourTurn
   case InvalidBid
-  case CardNotAllowed
+  case CardNotAllowed(reason: Reasons)
   case InvalidAction
--- src/main/scala/it/unibo/pps/wizard/engine/model/rules/TableRules.scala
+++ src/main/scala/it/unibo/pps/wizard/engine/model/rules/TableRules.scala
@@ -2,38 +2,40 @@ package it.unibo.pps.wizard.engine.model.rules
 
 import it.unibo.pps.wizard.engine.model.basic.*
 import it.unibo.pps.wizard.engine.model.core.GameError
-
-
-trait TableRules:
-  def validateCard(table: Table, hand: Hand, cardPlayed: Card, trump: Trump): Either[GameError, Unit]
-  def evaluateTrickWinner(table: Table, trump: Trump): PlayerId
+import it.unibo.pps.wizard.engine.model.core.Reasons.{CardNotInHand, MustFollowLeader}
 
 object TableRules:
-  def apply(): TableRules = new StandardWizardTableRules
 
-  private class StandardWizardTableRules extends TableRules:
+  extension (h: Hand)
+    private def hasColor(color: Card.Color): Boolean = h.toList.exists:
+        case Card.Standard(c, _) => c == color
+        case _                   => false
 
-    override def validateCard(table: Table, hand: Hand, cardPlayed: Card, trump: Trump): Either[GameError, Unit] =
-      if !hand.contains(cardPlayed) then return Left(GameError.CardNotAllowed)
-      val leaderColorOption = table.leaderCard.map(_.color)
-      (leaderColorOption, cardPlayed) match
-        case (Some(leaderColor), Card.Standard(c, _)) if c != leaderColor =>
-          val hasLeaderColor = hand.toList.exists:
-            case Card.Standard(color, _) => color == leaderColor
-            case _ => false
-          if hasLeaderColor then Left(GameError.CardNotAllowed) else Right(())
-        case _ => Right(())
+  extension (cardPlayed: Card)
+    def validateAgainst(table: Table, hand: Hand): Either[GameError, Unit] =
+      if !hand.contains(cardPlayed) then
+        return Left(GameError.CardNotAllowed(CardNotInHand))
 
-    override def evaluateTrickWinner(table: Table, trump: Trump): PlayerId =
+      cardPlayed match
+        case _: SpecialCard => Right(())
+        case Card.Standard(playedColor, _) =>
+          table.leaderCard match
+            case None => Right(())
+            case Some(Card.Standard(leaderColor, _)) =>
+              if playedColor == leaderColor then Right(())
+              else if hand.hasColor(leaderColor) then Left(GameError.CardNotAllowed(MustFollowLeader(leaderColor)))
+              else Right(())
+
+  extension (table: Table)
+    def evaluateTrickWinner(trump: Trump): PlayerId =
       val cards = table.playedCards
       val trumpColor = trump.effectiveColor
-      val leaderColor: Option[Card.Color] = table.leaderCard.map(_.color)
+      val leaderColor = table.leaderCard.map(_.color)
 
       def highestOf(targetColor: Option[Card.Color]): Option[Card] =
-        cards.collect:
-          case c@Card.Standard(color, _) if targetColor.contains(color) => c
-        .maxByOption(_.rank.ordinal)
-      
+        cards.collect { case c @ Card.Standard(color, rank) if targetColor.contains(color) => c }
+          .maxByOption(_.rank.value)
+
       val winningCard = cards.find(_.isInstanceOf[Card.Wizard])
         .orElse(highestOf(trumpColor))
         .orElse(highestOf(leaderColor))
