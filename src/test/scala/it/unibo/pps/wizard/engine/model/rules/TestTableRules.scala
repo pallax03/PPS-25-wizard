dea44b344efd7b0346eab5734d5e06e5ba8aa50e
alex mazzoni <alex.mazzoni3@studio.unibo.it>
2026-06-27 12:36:08 +0200

    refactor: TableRules fix opacity code

--- src/test/scala/it/unibo/pps/wizard/engine/model/rules/TestTableRules.scala
+++ src/test/scala/it/unibo/pps/wizard/engine/model/rules/TestTableRules.scala
@@ -29,19 +29,19 @@ class TestTableRules extends AnyWordSpec with Matchers:
       val c1: Card = 5.blue
       val c2: Card = 10.red
       val hand = (c1 - c2 - myWizard).asHand
-      "allow playing anything if there is no leader" in:
+      "allow playing anything if there is no cards" in:
         c1.validateAgainst(Table.empty, hand) shouldBe Right(())
 
-      "player HAS to follow the leader color" in:
+      "player HAS to follow the following color" in:
         val table = Table.empty + (p1 plays 4.blue)
         val result = c2.validateAgainst(table, hand)
         result shouldBe Left(GameError.CardNotAllowed(MustFollowLeader(Blue)))
 
-      "player LACKS the leader color" in:
+      "player LACKS the following color" in:
         val table = Table.empty + (p1 plays 4.yellow)
         c2.validateAgainst(table, hand) shouldBe Right(())
 
-      "always allow special cards even if player has the leader color" in:
+      "always allow special cards even if player has the following color" in:
         val table = Table.empty + (p1 plays 4.blue)
         myWizard.validateAgainst(table, hand) shouldBe Right(())
 
@@ -50,18 +50,17 @@ class TestTableRules extends AnyWordSpec with Matchers:
     val p2 = PlayerId(2)
     val p3 = PlayerId(3)
     "award the trick to the first Wizard played" in:
-      val firstWizard = wizard
-      val secondWizard = wizard
-      val table = Table.empty + (p1 plays 10.red) + (p2 plays firstWizard) + (p3 plays secondWizard)
+      val table = Table.empty + (p1 plays 10.red) + (p2 plays wizard) + (p3 plays wizard)
       table.evaluateTrickWinner(Trump.Absent) shouldBe p2
 
-    "award the trick to the highest Trump (no Wizard)" in:
+    "award the trick to the highest trump (no Wizard)" in:
       val table = Table.empty + (p1 plays 10.blue) + (p2 plays 2.red) + (p3 plays 5.red)
       table.evaluateTrickWinner(1.red.asTrump) shouldBe p3
 
-    "award the trick to the highest card (no Trump and no Wizard)" in:
+    "award the trick to the highest following card (no Trump and no Wizard)" in:
       val table = Table.empty + (p1 plays 5.blue) + (p2 plays 10.blue) + (p3 plays 2.yellow)
-      table.evaluateTrickWinner(1.green.asTrump) shouldBe p2 // trump color differ from played cards
+      val trump = 1.green.asTrump // trump color differ from played cards
+      table.evaluateTrickWinner(trump) shouldBe p2
 
     "award the trick to the first played Jester if ONLY Jesters are on table" in:
       val table = Table.empty + (p1 plays jester) + (p2 plays jester) + (p3 plays jester)
