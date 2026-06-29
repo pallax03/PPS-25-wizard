1aa1394bad3e03ea66ae47041ea3255defcf8ea3
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-06-29 16:31:51 +0200

    feat: implemented application and context

---
+++ src/main/scala/it/unibo/pps/wizard/application/WizardApplication.scala
@@ -0,0 +1,33 @@
+package it.unibo.pps.wizard.application
+
+import it.unibo.pps.wizard.application.WizardApplicationContext.WizardApplicationContextBuilder
+import it.unibo.pps.wizard.application.gui.pages.ViewManager
+import it.unibo.pps.wizard.engine.ports.WizardPort
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object WizardApplication extends JFXApp3:
+  private val contextBuilder: WizardApplicationContextBuilder = WizardApplicationContext.builder
+
+  given applicationContext: WizardApplicationContext = this.contextBuilder.build
+
+  def launch(wizardEngineProxy: WizardPort)(args: Array[String]): Unit =
+    Future:
+      this.contextBuilder.setWizardEngineProxy(wizardEngineProxy)
+      this.main(args)
+
+  override def start(): Unit =
+    stageConfiguration()
+    ViewManager(stage)
+
+  override def stopApp(): Unit =
+    System.exit(0)
+
+  private def stageConfiguration(): Unit =
+    this.stage = new PrimaryStage():
+      title = "PPS Card Game - Wizard"
+      resizable = false
+    this.contextBuilder.setPrimaryStage(this.stage)
--- src/main/scala/it/unibo/pps/wizard/application/gui/pages/ViewManager.scala
+++ src/main/scala/it/unibo/pps/wizard/application/gui/pages/ViewManager.scala
@@ -1,18 +1,14 @@
 package it.unibo.pps.wizard.application.gui.pages
 
 import cats.data.State
-import scalafx.application.JFXApp3
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
 import it.unibo.pps.wizard.engine.model.basic.*
+import scalafx.stage.Stage
 
-import scala.compiletime.uninitialized
+object ViewManager:
 
-object ViewManager extends JFXApp3:
-
-  private var mainScene: Scene = uninitialized
-
-  override def start(): Unit =
+  def apply(stage: Stage): Unit =
 
     val drawAction: State[Deck, (List[Card], Option[Card])] = for
       player1Cards <- Deck.pop(6)
@@ -22,15 +18,16 @@ object ViewManager extends JFXApp3:
     val deck = Deck.create
     val (playerCards1, trumpCard) = drawAction.runA(deck).value
 
+    var mainScene: Scene = null
+
     def handleStartGame(): Unit =
-      mainScene.root = new GameBoardView(Hand(playerCards1), Table.empty, Trump(trumpCard.head))
+      if mainScene != null then
+        mainScene.root = new GameBoardView(Hand(playerCards1), Table.empty, Trump(trumpCard.head))
 
     mainScene = new Scene:
       fill = Color.rgb(28, 28, 28)
       root = new HomeView(handleStartGame)
 
-    stage = new JFXApp3.PrimaryStage:
-      title = "PPS Card Game - Wizard"
-      width = 1200
-      height = 800
-      scene = mainScene
+    stage.width = 1200
+    stage.height = 800
+    stage.scene = mainScene
