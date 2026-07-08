a05fc7943d81b9cdf7312599c68d3e40e9ea4829
Alex Mazzoni <alex.mazzoni3@studio.unibo.it>
2026-07-08 21:49:32 +0200

    feat: table followingCard bug fixes

--- src/main/scala/it/unibo/pps/wizard/engine/model/basic/gameplay.scala
+++ src/main/scala/it/unibo/pps/wizard/engine/model/basic/gameplay.scala
@@ -16,10 +16,13 @@ object Table:
     def playerOf(card: Card): Option[PlayerId] = t.find(_._2 == card).map(_._1)
 
     def followingCard: Option[Card.Standard] =
-      t.playedCards
-        .dropWhile(_.isInstanceOf[Card.Jester])
-        .headOption
-        .collect { case s: Card.Standard => s }
+      if t.playedCards.exists(c => c.isInstanceOf[Card.Wizard]) then
+        Option.empty
+      else
+        t.playedCards
+          .dropWhile(_.isInstanceOf[Card.Jester])
+          .headOption
+          .collect { case s: Card.Standard => s }
 
     infix def +(play: (PlayerId, Card)): Table = t :+ play
 
