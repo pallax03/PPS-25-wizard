e73feb4d8ab6060d5eac77066e7775159dec631a
Alex Mazzoni <alex.mazzoni3@studio.unibo.it>
2026-07-09 16:47:34 +0200

    feat: DumbBotStrategy Random Bid

--- src/main/scala/it/unibo/pps/wizard/application/bot/strategy/DumbBotStrategy.scala
+++ src/main/scala/it/unibo/pps/wizard/application/bot/strategy/DumbBotStrategy.scala
@@ -2,7 +2,7 @@ package it.unibo.pps.wizard.application.bot.strategy
 
 import it.unibo.pps.wizard.application.bot.strategy.BotStrategy
 import it.unibo.pps.wizard.engine.events.InvitationEvent
-import it.unibo.pps.wizard.engine.model.basic.{Bid, Card}
+import it.unibo.pps.wizard.engine.model.basic.{Bid, Card, Round}
 import it.unibo.pps.wizard.engine.model.core.GameAction
 
 import scala.util.Random
@@ -11,7 +11,7 @@ class DumbBotStrategy(random: Random = Random()) extends BotStrategy:
 
   override def decide(invitation: InvitationEvent): GameAction = invitation match
     case InvitationEvent.WaitingForBid(context) =>
-      GameAction.PlaceBid(context.playerId, Bid(1))
+      GameAction.PlaceBid(context.playerId, Bid(random.between(Round.start.value, context.round.value)))
     case InvitationEvent.WaitingForCard(context) =>
       GameAction.PlayCard(context.playerId, context.legalCards.head)
     case InvitationEvent.WaitingForTrump(context) =>
