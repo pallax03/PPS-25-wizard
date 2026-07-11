95f1a0b519bf3508f7702ccf945959ab8d475ea6
Alex Mazzoni <alex.mazzoni3@studio.unibo.it>
2026-07-11 12:13:50 +0200

    feat: wizard prolog mapper to term

--- src/main/scala/it/unibo/pps/wizard/engine/prolog/WizardTermMapper.scala
+++ src/main/scala/it/unibo/pps/wizard/engine/prolog/WizardTermMapper.scala
@@ -3,11 +3,19 @@ package it.unibo.pps.wizard.engine.prolog
 import it.unibo.pps.wizard.engine.model.basic.*
 
 object WizardTermMapper:
-  private final val NO_VALUE: String = "none"
+  final val NO_VALUE: String = "none"
 
-  def cardsTerm(cards: List[Card]): String = cards.map(c)
+  def cardsTerm(cards: List[Card]): String = cards.map(cardTerm).mkString("[",",","]")
+  
   def cardTerm(card: Option[Card]): String = card.map(cardTerm).getOrElse(NO_VALUE)
+  
   def cardTerm(card: Card): String = card match
     case card: SpecialCard => card.getClass.getSimpleName.toLowerCase
     case Card.Standard(color, rank) => s"card(${rank.value},${colorTerm(color)})"
+
+  def trumpColorTerm(trump: Trump): String = colorTerm(trump.effectiveColor)
+  
+  def colorTerm(color: Option[Card.Color]): String = color.map(colorTerm).getOrElse(NO_VALUE)
+  
+  def colorTerm(color: Card.Color): String = color.toString.toLowerCase
 
--- src/test/scala/it/unibo/pps/wizard/engine/model/prolog/TestWizardTermMapper.scala
+++ src/test/scala/it/unibo/pps/wizard/engine/model/prolog/TestWizardTermMapper.scala
@@ -1,5 +1,41 @@
 package it.unibo.pps.wizard.engine.model.prolog
 
-class TestWizardTermMapper {
+import it.unibo.pps.wizard.engine.model.basic.Trump
+import it.unibo.pps.wizard.engine.model.basic.Card.*
+import it.unibo.pps.wizard.engine.prolog.WizardTermMapper.*
 
-}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class TestWizardTermMapper extends AnyWordSpec with Matchers:
+
+  "WizardTermMapper" when:
+    "mapping color to term" should:
+      "a valid color" in:
+        colorTerm(Blue) shouldBe "blue"
+        colorTerm(Red) shouldBe "red"
+
+      "not valid color" in:
+        colorTerm(Option.empty) shouldBe NO_VALUE
+
+    "mapping trump to term" should:
+      "Absent Trump No color" in:
+        val trump = Trump.Absent
+        trumpColorTerm(trump) shouldBe NO_VALUE
+      "Standard Trump have same color of the card" in :
+        val trump = Option(5.red).asTrump
+        trumpColorTerm(trump) shouldBe "red"
+
+    "mapping card to term" should:
+      "not valid card" in:
+        cardTerm(Option.empty) shouldBe NO_VALUE
+      "a special card" in:
+        cardTerm(jester) shouldBe "jester"
+        cardTerm(wizard) shouldBe "wizard"
+      "a standard card" in :
+          cardTerm(5.red) shouldBe "card(5,red)"
+
+    "mapping cards to term" should:
+      val cards = 5.red - wizard - jester
+      "a list of valid cards" in:
+        cardsTerm(cards) shouldBe "[card(5,red),wizard,jester]"
