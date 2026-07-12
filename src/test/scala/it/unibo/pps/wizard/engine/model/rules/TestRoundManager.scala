98b6328acf1871833a41841567e588f8f76a3c69
Francesco Marcatelli <francesco.marcatelli@studio.unibo.it>
2026-07-12 16:52:21 +0200

    test: added test to verify the correctness of the Trump extraction

--- src/test/scala/it/unibo/pps/wizard/engine/model/rules/TestRoundManager.scala
+++ src/test/scala/it/unibo/pps/wizard/engine/model/rules/TestRoundManager.scala
@@ -55,6 +55,16 @@ class TestRoundManager extends AnyWordSpec with Matchers:
         trump shouldBe empty
         deckAfter.length shouldBe 0
         maxRound.isLastRound(players) shouldBe true
+        
+      "popped trump should not be in deck or in any player's hand" in:
+        val initialDeck = Deck.create
+        val round = Round.start.next.next.next.next.next.next
+        val (deckAfter, (hands, trump)) = round.deal(players).run(initialDeck).value
+
+        trump.foreach { t =>
+          deckAfter.cards should not contain t
+          hands.toList.flatMap(_._2.toList).toSet should not contain t
+        }
 
     "validating the turn of a player" should:
       "succeed if the action player matches the expected player" in:
