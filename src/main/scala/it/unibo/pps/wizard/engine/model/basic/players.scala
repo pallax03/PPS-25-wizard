7b43e57aa1cf7081c80b1a8ff5b01a95f2885efe
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-07-13 13:41:58 +0200

    feat: added extension method to Players

--- src/main/scala/it/unibo/pps/wizard/engine/model/basic/players.scala
+++ src/main/scala/it/unibo/pps/wizard/engine/model/basic/players.scala
@@ -32,3 +32,4 @@ object Players:
     def toList: List[Player] = players
     def totalPlayers: Int = players.size
     def filter(predicate: Player => Boolean): Players = players.filter(predicate)
+    def findById(id: PlayerId): Option[Player] = players.find(_.id == id)
