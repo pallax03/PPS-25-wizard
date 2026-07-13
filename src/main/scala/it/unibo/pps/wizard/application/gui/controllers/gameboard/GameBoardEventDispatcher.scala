d6cd5a06938653b84d7573f98b9f68a925e2b137
Nicola Graziotin <nicola.graziotin@studio.unibo.it>
2026-07-13 13:40:49 +0200

    feat: added GameStarted event handler

--- src/main/scala/it/unibo/pps/wizard/application/gui/controllers/gameboard/GameBoardEventDispatcher.scala
+++ src/main/scala/it/unibo/pps/wizard/application/gui/controllers/gameboard/GameBoardEventDispatcher.scala
@@ -1,20 +1,19 @@
 package it.unibo.pps.wizard.application.gui.controllers.gameboard
 
 import it.unibo.pps.wizard.application.WizardApplicationContext
-import it.unibo.pps.wizard.engine.events.{ActionEvent, InvitationEvent, ProgressEvent}
+import it.unibo.pps.wizard.engine.events.{ActionEvent, InvitationEvent, LifecycleEvent, ProgressEvent, WizardEvent}
 import scalafx.application.Platform
 
 class GameBoardEventDispatcher(private val view: GameBoardView)(using
     context: WizardApplicationContext
 ):
   def startListening(): Unit =
-    context.inboundPort.subscribe[ActionEvent]:
+    context.inboundPort.subscribe[WizardEvent]:
       case ActionEvent.CardPlayed(playerId, card) => runOnUi(view.displayCardPlayed(playerId, card))
       case ActionEvent.TrumpColorResolved(playerId, color) =>
         runOnUi(view.displayTrumpSelected(playerId, color))
       case ActionEvent.BidPlaced(playerId, bid) => runOnUi(view.displayBidPlaced(playerId, bid))
 
-    context.inboundPort.subscribe[ProgressEvent]:
       case ProgressEvent.CardsDealt(playerId, hands, trump, round) =>
         runOnUi(view.displayCardsDealt(playerId, hands, trump, round))
       case ProgressEvent.TrickWon(winnerId, trickedCards) =>
@@ -23,10 +22,12 @@ class GameBoardEventDispatcher(private val view: GameBoardView)(using
       case ProgressEvent.PhaseChanged(phase)     => runOnUi(view.displayPhaseChanged(phase))
       case ProgressEvent.IsTurnOf(playerId, phase)      => runOnUi(view.displayTurnChanged(playerId, phase))
 
-    context.inboundPort.subscribe[InvitationEvent]:
       case InvitationEvent.WaitingForTrump(playerId) =>
         runOnUi(view.displayWaitingForTrump(playerId))
-      case _ =>
+      case InvitationEvent.WaitingForBid(playerId, round) => ???
+
+      case LifecycleEvent.GameStarted(players, _) =>
+        runOnUi(view.displayGameStarted(players))
 
   private def runOnUi(action: => Unit): Unit =
     Platform.runLater(action)
